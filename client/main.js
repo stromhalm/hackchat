da845e64644524634c442e6ecfdd2ae4c72600b6
hendrik <hendrik.kahlen@gmail.com>
2016-10-01 00:28:49 +0200

    assign added

--- client/main.js
+++ client/main.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import messagesList from '../imports/components/messagesList/messagesList';
- 
+import assign from '../imports/components/assign/assign';
+
 angular.module('hackchat', [
   angularMeteor,
-  messagesList.name
+  messagesList.name,
+  assign.name
 ]);
